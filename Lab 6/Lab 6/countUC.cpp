712133eeb049b8c49a68734ff12d5c72a46ac7f4
Unknown <zacharyd1@live.com>
2017-10-05 00:24:41 -0400

    Completed Exercise 1

--- Lab 6/Lab 6/countUC.cpp
+++ Lab 6/Lab 6/countUC.cpp
@@ -4,11 +4,20 @@
 #include <iostream>
 using namespace std;
 
-int main ()
-{  
-  char letter;
-  int  letterCt;
+int main()
+{
+	string input = "AbbaDabbaDoo";
+	int numCapitals = 0;
 
-  /* TO BE FILLED IN */
-  return 0;
+	for (auto iter = input.begin(); iter != input.end(); iter++)	//Loop over every letter
+	{
+		if ('A' <= *iter && *iter <= 'Z') //If the current letter is a capital
+			numCapitals++;
+	}
+
+	cout << "The number of capitals:" << numCapitals << endl;
+
+	cin.get();
+
+	return 0;
 }
