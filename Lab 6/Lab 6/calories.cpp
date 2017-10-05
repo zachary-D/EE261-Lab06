22fb9d8af3179e675b9e241399bd001ca43cc86e
Unknown <zacharyd1@live.com>
2017-10-04 23:59:25 -0400

    Rolled my work on calories.cpp forward from lab 5

--- Lab 6/Lab 6/calories.cpp
+++ Lab 6/Lab 6/calories.cpp
@@ -5,25 +5,29 @@
 #include <string>
 using namespace std;
 
-int main ()
+int main()
 {
-  string foodItem;
-  int   gramsOfFat;
-  int   calories;
-  float fatCalPercent;
+	string foodItem;
+	int   gramsOfFat;
+	int   calories;
+	float fatCalPercent;
 
-  cout << "Enter the name of a food item."  
-       << "  Press return."  << endl;
-  getline(cin, foodItem);
+	cout << "Enter the name of a food item."
+		<< "  Press return." << endl;
+	getline(cin, foodItem);
 
-  cout << "Enter the grams of fat; press return."  << endl;
-  cin >> gramsOfFat;
+	cout << "Enter the grams of fat; press return." << endl;
+	cin >> gramsOfFat;
 
-  cout << "Enter the number of calories; press return."
-       << endl;
-  cin >> calories;
-  
-  /* TO BE FILLED IN */
-  return 0;
+	cout << "Enter the number of calories; press return."
+		<< endl;
+	cin >> calories;
+
+	fatCalPercent = ((float)gramsOfFat * 9) / (float)calories * 100;
+
+	cout << "Fat calorie percentage:" << fatCalPercent << "%" << endl;
+
+	cout << "This item is " << ((fatCalPercent < .3) ? "NOT " : "") << "Heart Healthy!!" << endl;
+	cin.get(); cin.get(); //Hold the window open
+	return 0;
 }
-
